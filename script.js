ee9c3eb0a709aac2ab11d5e355ed961c22ace527
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-07-06 22:13:33 -0400

    Added terms and conditions check

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let signUpBtn = document.querySelector("#sign-up-btn");
 let pswdBtn = document.querySelector("#pswd");
 let pswdConfBtn = document.querySelector("#pswd-conf");
 let errorP = document.querySelector(".non-match");
+let termsCheck = document.querySelector("#terms");
 
 signUpBtn.addEventListener("click", (e) => {
     let pswrd = pswdBtn.value;
@@ -14,7 +15,13 @@ signUpBtn.addEventListener("click", (e) => {
         console.log("3");
         pswdConfBtn.value = "";
         errorP.textContent = "🚩 Your passwords must match";
-    } else {
-        errorP.textContent = "";
+        return;
     }
+
+    if (termsCheck.checked == false) {
+        errorP.textContent = "🚩 You have not accepted the terms and conditions";
+        return;
+    }
+
+    errorP.textContent = "";
 })
